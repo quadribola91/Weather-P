f0fbd78d6410449536ba512f99c3430fc83b3f03
omobolarinwaquadri <quadribola91@gmail.com>
2024-02-17 19:08:51 +0100

    version112

--- script.js
+++ script.js
@@ -31,17 +31,19 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 
     // Update weather icon based on weather condition
+
     const weatherCondition = data.weather[0].main.toLowerCase();
+
     if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
+      weatherIcon.src = "./images/clouds.png";
     } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
+      weatherIcon.src = "./images/clear.png";
     } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
+      weatherIcon.src = "./images/rain.png";
     } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
+      weatherIcon.src = "./images/drizzle.png";
     } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
+      weatherIcon.src = "./images/mist.png";
     }
 
     document.querySelector(".weather").classList.remove("hidden");
